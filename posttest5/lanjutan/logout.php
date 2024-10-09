ba62ee6be6201de53fb5d2f17aa358987edad905
Chandra <152131475+ChandraPahlawan@users.noreply.github.com>
2024-10-10 07:40:36 +0800

    Up logout.php

--- posttest5/lanjutan/logout.php
+++ posttest5/lanjutan/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy(); // Hancurkan semua session
-header("Location: index.php"); // Arahkan kembali ke halaman login
+header("Location: ../index.php"); // Arahkan kembali ke halaman login
 exit();
 ?>
